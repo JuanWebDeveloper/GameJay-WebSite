47b6f7ca783b66c9ab5bb4fdd35b34d42f89aa9f
Juan Vargas <juandeveloper19@gmail.com>
2021-09-16 04:41:09 -0500

    Updating of styles and images and improvements in the code

--- js/homePage.js
+++ js/homePage.js
@@ -1,90 +1,91 @@
 /*--=====================================================
 |-Variables                                     |
 =======================================================--*/
-let acceder = document.getElementById("login");
-let registrar = document.getElementById("registrar");
+let acceder = document.getElementById("acceder");
+let registro = document.getElementById("registro");
+let olvido = document.getElementById("olvido");
 let EL = document.getElementById("EL");
 let recuperarContraseña = document.getElementById("recuperarContraseña");
-let colorL = document.getElementById("colorL");
-let colorR = document.getElementById("colorR");
-let activeEffectNavigation = $('.containerMenu nav ul li a');
+let login = document.getElementById("login");
+let registrar = document.getElementById("registrar");
+let activeEffectNavigation = $(".containerMenu nav ul li a");
+let containerMenu = document.getElementById("containerMenu");
+let bars = document.getElementById("bars");
+let navbar = document.getElementById("navbar");
 
 /*--=====================================================
 |- Animacion Login y Registro                           |
 =======================================================--*/
-colorL.style.color = "#fff";
-colorR.style.color = "#0077ff";
+acceder.style.color = "#fff";
+registro.style.color = "#0077ff";
 
-function login() {
-    acceder.style.left = "5%";
-    acceder.style.top = "20px";
-    acceder.style.display = "block";
-    registrar.style.left = "105%";
-    registrar.style.top = "20px";
-    recuperarContraseña.style.top = "105%";
-    EL.style.left = "0px";
-    colorL.style.color = "#fff";
-    colorR.style.color = "#0077ff";
-}
+registrar.classList.add("ocultar-registro");
+recuperarContraseña.classList.add("ocultar-recuperar");
 
-function registro() {
-    acceder.style.left = "-100%";
-    registrar.style.left = "5%";
-    acceder.style.top = "20px";
-    registrar.style.top = "20px";
-    recuperarContraseña.style.top = "100%";
-    EL.style.left = "50%";
-    colorL.style.color = "#0077ff";
-    colorR.style.color = "#fff";
-}
+acceder.addEventListener("click", () => {
+	acceder.style.color = "#fff";
+	registro.style.color = "#0077ff";
+	EL.style.left = "0%";
 
-function recuperar() {
-    acceder.style.top = "-100%";
-    acceder.style.display = "none";
-    registrar.style.top = "100%";
-    recuperarContraseña.style.top = "50px";
-}
+	registrar.classList.remove("mostrar-registro");
+	registrar.classList.add("ocultar-registro");
+
+	recuperarContraseña.classList.remove("mostrar-recuperar");
+	recuperarContraseña.classList.add("ocultar-recuperar");
+
+	login.classList.remove("ocultar-iniciar");
+	login.classList.add("mostrar-iniciar");
+});
+
+registro.addEventListener("click", () => {
+	acceder.style.color = "#0077ff";
+	registro.style.color = "#fff";
+	EL.style.left = "40%";
+
+	recuperarContraseña.classList.remove("mostrar-recuperar");
+	recuperarContraseña.classList.add("ocultar-recuperar");
+
+	login.classList.remove("mostra-iniciar");
+	login.classList.add("ocultar-iniciar");
+
+	registrar.classList.remove("ocultar-registro");
+	registrar.classList.add("mostrar-registro");
+});
+
+olvido.addEventListener("click", () => {
+	recuperarContraseña.classList.remove("ocultar-recuperar");
+	recuperarContraseña.classList.add("mostrar-recuperar");
+
+	login.classList.remove("mostra-iniciar");
+	login.classList.add("ocultar-iniciar");
+
+	registrar.classList.remove("mostra-registro");
+	registrar.classList.add("ocultar-registro");
+});
 
 /*--=====================================================
 |-Navbar Effect                                         |
 =======================================================--*/
-$(window).scroll(function(){
-
-	if ($(window).scrollTop() >= 40) {
-
-		$('.containerMenu').addClass('containerNavbar-scroll');
-
-	}else{
-
-		$('.containerMenu').removeClass('containerNavbar-scroll');
-
+window.onscroll = () => {
+	if (window.scrollY >= 40) {
+		containerMenu.classList.add("containerNavbar-scroll");
+	} else {
+		containerMenu.classList.remove("containerNavbar-scroll");
 	}
-
-});
+};
 
 /*--=====================================================
 |-Navbat Active Effect                                   |
 =======================================================--*/
-activeEffectNavigation.click(function(){
-
-    if($(this).hasClass('active')){
-
-        $(this).removeClass('active');
-
-    }else{
-
-        activeEffectNavigation.removeClass('active');
-        $(this).addClass('active');
-
-    }
-
-});
+activeEffectNavigation.click(() => navbar.classList.remove("show-menu"));
 
 /*--=====================================================
 |-Navbar Responsive                                      |
 =======================================================--*/
-$("#bars").click(function(){
-
-	$("#bars").after($(".containerMenu nav").slideToggle("slow"));
-
+bars.addEventListener("click", () => {
+	if (navbar.classList.contains("show-menu")) {
+		navbar.classList.remove("show-menu");
+	} else {
+		navbar.classList.add("show-menu");
+	}
 });
