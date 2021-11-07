f54bec1e328604402f9cba6142ce9b53e76d078f
Juan Vargas <juandeveloper19@gmail.com>
2021-11-07 13:43:04 -0500

    Footer redesign

--- views/modules/footer.php
+++ views/modules/footer.php
@@ -1,70 +1,26 @@
 <footer class="footer">
-	<div class="main-content">
-		<div class="left box">
-			<h2>Nosotros</h2>
-			<div class="box-content">
-				<p>
-					GAMES JAY fue fundada en enero del 2020 por los estudiantes de la institución educativa San
-					Pablo Paula Andrea Álvarez Sánchez, Juan Felipe Pérez Peña y Juan Fernando Vargas con el
-					propósito de crear y distribuir juegos pedagógicos con temáticas de problemáticas sociales.
-				</p>
-				<div class="footer-social_networks">
-					<a href="#"><i class="fab fa-facebook"></i></a>
-					<a href="#"><i class="fab fa-instagram"></i></a>
-					<a href="#"><i class="fab fa-twitter"></i></a>
-					<a href="#"><i class="fab fa-youtube"></i></a>
-				</div>
-			</div>
+	<div class="footer-content">
+		<div class="footer-logo_container">
+			<img src="<?php echo $url; ?>accessories/img/logo.png" alt="Logo de GameJay">
 		</div>
 
-		<div class="center box">
-			<h2>Informacion de contacto</h2>
-			<div class="box-content">
-				<div class="place">
-					<span class="fas fa-map-marker-alt"></span>
-					<span class="text">97 ##36-38, Medellín, Antioquia</span>
-				</div>
-
-				<div class="phone">
-					<span class="fas fa-phone-alt"></span>
-					<span class="text">5213098</span>
-				</div>
-
-				<div class="place">
-					<span class="fas fa-envelope"></span>
-					<span class="text">gamesjay@gmail.com</span>
-				</div>
-			</div>
+		<div class="social-media">
+			<a href="https://wa.link/xpq33u" target="_blank">
+			   <i class="fab fa-whatsapp"></i>
+			</a> 
+        	<a href="https://www.instagram.com/juanvargas_wd/" target="_blank">
+				<i class="fab fa-instagram"></i>
+			</a>
+			<a href="https://www.linkedin.com/in/juan-fernando-vargas-garces-a4a22720b/" target="_blank">
+				<i class="fab fa-linkedin"></i>
+			</a>
+			<a href="https://github.com/JuanWebDeveloper" target="_blank">
+				<i class="fab fa-github-square"></i>
+			</a>
 		</div>
 
-		<div class="right box">
-			<h2>Contacto</h2>
-			<div class="box-content">
-				<form action="#">
-					<div class="email">
-						<div class="text"><span>Correo</span></div>
-						<input type="email" />
-					</div>
-
-					<div class="msg">
-						<div class="text"><span>Mensaje</span></div>
-						<textarea></textarea>
-					</div>
-
-					<div class="btn">
-						<button type="submit">Enviar</button>
-					</div>
-				</form>
-			</div>
+		<div class="footer-copy_content">
+			<p>Creada y desarrollada por <a href="https://github.com/JuanWebDeveloper" target="_blank"">JuanWebDeveloper</a> &#169 2021 todos los derechos reservados.</p>
 		</div>
-	</div>
-
-	<div class="bottom">
-		<center>
-			<span class="credit"
-				>Creada por <a href="#">Game<span>Jay</span></a> |
-			</span>
-			<span class="far fa-copyright"></span><span> 2020 Todos los derechos reservados.</span>
-		</center>
 	</div>
 </footer>
