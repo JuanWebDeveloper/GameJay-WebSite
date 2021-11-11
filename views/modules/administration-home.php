ee51924c40df6b118352bd7cafe5f2dd7ed43c95
Juan Vargas <juandeveloper19@gmail.com>
2021-11-11 16:27:02 -0500

    Administrative panel startup redesign

--- views/modules/administration-home.php
+++ views/modules/administration-home.php
@@ -1,3 +1,5 @@
-<div class="admin-home-content">
-    <h2>Bienvenido al panel de control de Game<span>Jay</span></h2>
+<div class="admin-home" id="particles-js">
+    <div class="admin-home-content">
+        <h2>Bienvenido al panel de control de Game<span>Jay</span></h2>
+    </div>
 </div>
