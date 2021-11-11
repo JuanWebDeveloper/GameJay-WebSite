c8b2bf8de283663b220bb86d793928f7522086af
Juan Vargas <juandeveloper19@gmail.com>
2021-11-11 16:13:06 -0500

    Redesign of login as administrator

--- views/modules/login-admin.php
+++ views/modules/login-admin.php
@@ -1,12 +1,22 @@
 <div class="login-admin">
 	<div class="login-admin-content">
 		<h2 class="main-titles"><span>Administrador</span></h2>
-		<form method="POST" class="form-admin">
-			<input type="text" name="email" class="input-field" placeholder="Correo" />
-			<input type="password" name="password" class="input-field" placeholder="Contarseña" />
-			<input type="hidden" name="admin" value="admin" />
-			<button type="submit" style="margin-top: 20px" class="submit-button">Iniciar</button>
-		</form>
+		<div class="form-contact">
+			<form method="POST" class="form-box">
+				<div class="input-box width-100">	
+					<input type="text" name="email" required autocomplete="nope" />
+					<span>Correo</span>
+				</div>
+				<div class="input-box width-100">
+					<input type="password" name="password" required />
+					<span>Contraseña</span>
+				</div>
+				<input type="hidden" name="admin" value="admin" />
+				<div class="form-button_container">
+                    <button type="submit" class="button form-button"><span>Iniciar Sesión</span></button>
+                </div>
+			</form>
+		</div>
 	</div>
 	<?php 
 		$admin = "";
